04e84c915bb4a1332d47fcbf6f15d9f57bdc8347
Pragati <pragatibhattad1610@gmail.com>
2020-03-30 23:20:33 +0530

    search by country

--- app_express.js
+++ app_express.js
@@ -11,40 +11,49 @@ const URL = "https://coronavirus-tracker-api.herokuapp.com/v2/locations";
 
 app.use(cors());
 
-router.get('/',async(req,res)=>{   
-        let result;
-        try {
-            var key = "randomString";
-    client.get(key,async function(err, reply){
-        res.setHeader('Content-Type', 'application/json; charset=utf-8');
-        if(reply){
-            
-            res.write(reply);
-            
-            var data = await apiCallFromNode.callApi();
-            client.set(key, data);
-        }else{
-            
-            var data = await apiCallFromNode.callApi();
-            res.write(JSON.stringify(data)); 
-                                   //waits for all the chunks of responses the server 
-                                            //provides to our requests and then ends the connection
-            client.on("error", function(error) {
-                console.error(error);
-            });
-            // these are unnecessary
-            // client.set("key", dataString, redis.print);
-            // client.get("key", redis.print);
-        }
-    }); 
-            result = await axios.get(URL);
-        } catch(err) {
-            console.log(err);
-        }
-        res.write(JSON.stringify(result.data));
-        res.end();
+function countryFilter(item, countryName) {
+    const e = item.country;
+    return (
+      e.includes(countryName)
+    );
+  }
+
+router.get('/countryName', async (req, res) => {
+    let result;
+    try {
+        var key = "randomString";
+        client.get(key, async function (err, reply) {
+            res.setHeader('Content-Type', 'application/json; charset=utf-8');
+            if (reply) {
+
+                res.write(reply);
+
+                var data = await apiCallFromNode.callApi();
+                client.set(key, data);
+            } else {
+
+                var data = await apiCallFromNode.callApi();
+                res.write(JSON.stringify(data));
+                //waits for all the chunks of responses the server 
+                //provides to our requests and then ends the connection
+                client.on("error", function (error) {
+                    console.error(error);
+                });
+                // these are unnecessary
+                // client.set("key", dataString, redis.print);
+                // client.get("key", redis.print);
+            }
+        });
+        result = await axios.get(URL);
+        var item = countryFilter(result, req.body.countryName);
+
+    } catch (err) {
+        console.log(err);
+    }
+    res.write(JSON.stringify(item.data));
+    res.end();
 });
 
-app.use("/node",router);
+app.use("/node", router);
 
 module.exports = app;
